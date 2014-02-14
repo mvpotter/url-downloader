9be6f47b5a68055f79dbd5767e612210d04a2207
mvpotter <supermegapotter@gmail.com>
2014-02-14 13:25:10 +0700

    Lauch from console is implemented

--- url_downloader.py
+++ url_downloader.py
@@ -1,13 +1,14 @@
 import os
 import sys
 import getopt
+import argparse
 import urllib.request
 
 class UrlDownloader:
 
 	def download(self, dest_dir, links):
-		if not os.path.exists(dest_directory):
-			os.makedirs(dest_directory)
+		if not os.path.exists(dest_dir):
+			os.makedirs(dest_dir)
 
 		for link in links:
 			last_slash_index = link.rfind("/") 
@@ -16,22 +17,45 @@ class UrlDownloader:
 				print ("Unable to recognize filename")
 				continue
 			if last_dot_index == -1 or last_dot_index < last_slash_index:
-				print ("Unable to recognize file extension for " + link)
+				print ("Unable to recognize file extension for {}".format(link))
 				continue
 			downloaded_file_name = link[last_slash_index + 1:]
-			urllib.request.urlretrieve(link, "{0}/{1}".format(dest_directory, downloaded_file_name))
-			print ("File is saved: " + downloaded_file_name)
+			try:
+				urllib.request.urlretrieve(link, "{0}/{1}".format(dest_dir, downloaded_file_name))
+				print ("File is saved: {}".format(downloaded_file_name))
+			except ValueError as e:
+				print (e)
+			except urllib.error.HTTPError as e:
+				print ("{0} for {1}".format(e, link))
+
 			
 if __name__ == "__main__":
-	config = open("config.txt", "r");
-	dest_directory = "downloads"
+	DEFAULT_DOWNLOAD_DIR = 'downloads'
+
+	parser = argparse.ArgumentParser(description='Downloads files from the web located by links.')
+	parser.add_argument('-l','--links', nargs='*', help='Links of files to be downloaded', metavar='links', required=False)
+	parser.add_argument('-c','--config', help='Configuration file with links (each link on a single line)', metavar='config_file', required=False)
+	parser.add_argument('-d','--destination', help='Destination directory', default=DEFAULT_DOWNLOAD_DIR, metavar='destination_dir', required=False)
+	args = parser.parse_args()
+	
 	links = []
-	for line in config:
-		links.append(line.rstrip('\n'))
+	if args.links:
+		for link in args.links:
+			links.append(link)
+	if args.config:
+		try :
+			config = open(args.config, "r");
+			for line in config:
+				links.append(line.rstrip('\n'))
+		except IOError:
+			print ("Unable to open file {}".format(args.config))
+			exit(1)	
+	if len(links) == 0:
+		print ("No links to download were provided")
+		exit(2)
 
 	url_downloader = UrlDownloader()
-	url_downloader.download(dest_directory, links)
-
+	url_downloader.download(args.destination, links)
 
 
 
